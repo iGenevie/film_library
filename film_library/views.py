b89e8c56410b36019e65d07e77fa1f4bed40c775
Aleksandr <sashabadretdinov@mail.ru>
2019-04-10 01:31:20 +0300

    Created films_list view

--- film_library/views.py
+++ film_library/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from .models import User
+from django.shortcuts import get_object_or_404
+from .models import User, Film
 
 
 def users_list(request):
     users = User.objects.all()
     return render(request, 'film_library/users_list.html', {'users': users})
+
+
+def films_list(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    films = Film.objects.filter(user=user)
+    return render(request, 'film_library/films_list.html', {'films': films, 'user': user})
