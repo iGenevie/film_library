3babf1eada431a74fa5599d1df49880e5f954e92
Aleksandr <sashabadretdinov@mail.ru>
2019-04-10 01:47:03 +0300

    Created user_new view

--- film_library/views.py
+++ film_library/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from .models import User, Film
+from .forms import UserForm
+from django.utils import timezone
 
 
 def users_list(request):
     users = User.objects.all()
     return render(request, 'film_library/users_list.html', {'users': users})
+
+
+def user_new(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.created_date = timezone.now()
+            user.save()
+            return redirect('users_list')
+    else:
+        form = UserForm()
+    return render(request, 'film_library/user_new.html', {'form': form})
 
 
 def films_list(request, pk):
