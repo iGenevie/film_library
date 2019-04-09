2f8d9474787963c30d5cddb086e653529a5ec0db
Aleksandr <sashabadretdinov@mail.ru>
2019-04-10 01:19:30 +0300

    Created users_list view

--- film_library/views.py
+++ film_library/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+
+def users_list(request):
+    users = User.objects.all()
+    return render(request, 'film_library/users_list.html', {'users': users})
