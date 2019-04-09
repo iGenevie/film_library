d69bf47197c5d79119a1f80d91ed7d793475654e
Aleksandr <sashabadretdinov@mail.ru>
2019-04-10 02:04:50 +0300

    Added film_new in urls

--- film_library/urls.py
+++ film_library/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.users_list, name='users_list'),
     path('films/<int:pk>/', views.films_list, name='films_list'),
     path('user/new/', views.user_new, name='user_new'),
+    path('films/<int:user_pk>/new/', views.film_new, name='film_new'),
 ]
