e540f879520a98a13abcaaaaa53153f79d136920
Aleksandr <sashabadretdinov@mail.ru>
2019-04-10 01:55:24 +0300

    Added FilmForm

--- film_library/forms.py
+++ film_library/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import User
+from .models import User, Film
 
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('login',)
+
+
+class FilmForm(forms.ModelForm):
+    class Meta:
+        model = Film
+        fields = ('original_title', 'production', 'country', 'year',)
