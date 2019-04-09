7817487f2c79c5e3190781860cc8a67816a5b03b
Aleksandr <sashabadretdinov@mail.ru>
2019-04-09 23:46:34 +0300

    Created User model

--- film_library/models.py
+++ film_library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    login = models.CharField(max_length=15)
+    created_date = models.DateTimeField(timezone.now())
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.login
