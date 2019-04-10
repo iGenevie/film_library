823f57c0a7e8ac8309907d977659427b07a7f999
Aleksandr <sashabadretdinov@mail.ru>
2019-04-10 20:13:58 +0300

    Created tests for User and Film models

--- film_library/tests.py
+++ film_library/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import User, Film
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.mary = User.objects.create(login='Mary', created_date=timezone.now())
+        self.vladislav = User.objects.create(login='Vladislav', created_date=timezone.now())
+
+    def test_users_str(self):
+        self.assertEqual(self.mary.__str__(), 'Mary')
+        self.assertEqual(self.vladislav.__str__(), 'Vladislav')
+
+
+class FilmTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(login='user', created_date=timezone.now())
+        self.garfield = Film.objects.create(
+            user=user,
+            original_title='Garfield',
+            production="Davis Entertainment Company",
+            country="United States",
+            year='2004',
+            added_date=timezone.now()
+        )
+
+    def test_film_str(self):
+        self.assertEqual(self.garfield.__str__(), 'Garfield')
